5ceed3102eea1f7322a07567c0a6a1ec00d9cb05
Jerickson <jericksonmayor@gmail.com>
2024-04-17 12:53:42 +0800

    modify mw-proxy to add mu38 support

---
+++ src/routes/index.ts
@@ -0,0 +1,66 @@
+import { getBodyBuffer } from '@/utils/body';
+import {
+  extractHeadersFromPath,
+  getAfterResponseHeaders,
+  getBlacklistedHeaders,
+  getProxyHeaders
+} from '@/utils/headers';
+import {
+  createTokenIfNeeded,
+  isAllowedToMakeRequest,
+  setTokenHeader,
+} from '@/utils/turnstile';
+
+export default defineEventHandler(async (event) => {
+  // handle cors, if applicable
+  if (isPreflightRequest(event)) return handleCors(event, {});
+
+
+  // parse destination URL
+  const destination = getQuery<{ destination?: string }>(event).destination;
+  
+  if (!destination)
+    return await sendJson({
+      event,
+      status: 200,
+      data: {
+        message: `Proxy is working as expected (v${
+          useRuntimeConfig(event).version
+        })`,
+      },
+    });
+
+  if (!(await isAllowedToMakeRequest(event)))
+    return await sendJson({
+      event,
+      status: 401,
+      data: {
+        error: 'Invalid or missing token',
+      },
+    });
+
+  // read body
+  const body = await getBodyBuffer(event);
+  const token = await createTokenIfNeeded(event);
+  console.log(extractHeadersFromPath(event));
+
+  // proxy
+  try {
+    await specificProxyRequest(event, destination, {
+      blacklistedHeaders: getBlacklistedHeaders(),
+      fetchOptions: {
+        redirect: 'follow',
+        headers: getProxyHeaders(event.headers, extractHeadersFromPath(event)),
+        body,
+      },
+      async onResponse(outputEvent, response) {
+        const headers = getAfterResponseHeaders(response.headers, response.url);
+        setResponseHeaders(outputEvent, headers);
+        if (token) setTokenHeader(event, token);
+      },
+    });
+  } catch (e) {
+    console.log('Error fetching', e);
+    throw e;
+  }
+});
---
+++ src/utils/proxy.ts
@@ -0,0 +1,150 @@
+import {
+  extractHeadersFromPath,
+} from '@/utils/headers';
+import {
+  Duplex,
+  H3Event,
+  ProxyOptions,
+  RequestHeaders,
+  getProxyRequestHeaders,
+} from 'h3';
+
+const PayloadMethods = new Set(['PATCH', 'POST', 'PUT', 'DELETE']);
+
+export interface ExtraProxyOptions {
+  blacklistedHeaders?: string[];
+}
+
+function mergeHeaders(
+  defaults: HeadersInit,
+  ...inputs: (HeadersInit | RequestHeaders | undefined)[]
+) {
+  const _inputs = inputs.filter(Boolean) as HeadersInit[];
+  if (_inputs.length === 0) {
+    return defaults;
+  }
+  const merged = new Headers(defaults);
+  for (const input of _inputs) {
+    if (input.entries) {
+      for (const [key, value] of (input.entries as any)()) {
+        if (value !== undefined) {
+          merged.set(key, value);
+        }
+      }
+    } else {
+      for (const [key, value] of Object.entries(input)) {
+        if (value !== undefined) {
+          merged.set(key, value);
+        }
+      }
+    }
+  }
+  return merged;
+}
+
+export async function specificProxyRequest(
+  event: H3Event,
+  target: string,
+  opts: ProxyOptions & ExtraProxyOptions = {},
+) {
+  let body;
+  let duplex: Duplex | undefined;
+  if (PayloadMethods.has(event.method)) {
+    if (opts.streamRequest) {
+      body = getRequestWebStream(event);
+      
+      duplex = 'half';
+    } else {
+      body = await readRawBody(event, false).catch(() => undefined);
+      
+    }
+  }
+
+  const method = opts.fetchOptions?.method || event.method;
+  const oldHeaders = getProxyRequestHeaders(event);
+  opts.blacklistedHeaders?.forEach((header) => {
+    const keys = Object.keys(oldHeaders).filter(
+      (v) => v.toLowerCase() === header.toLowerCase(),
+    );
+    keys.forEach((k) => delete oldHeaders[k]);
+  });
+
+  const fetchHeaders = mergeHeaders(
+    oldHeaders,
+    opts.fetchOptions?.headers,
+    opts.headers,
+  );
+  const headerObj = Object.fromEntries([...(fetchHeaders.entries as any)()]);
+  if (process.env.REQ_DEBUG === 'true') {
+    console.log({
+      type: 'request',
+      method,
+      url: target,
+      headers: headerObj,
+    });
+  }
+
+    return sendProxy(event, target, {
+    ...opts,
+    fetchOptions: {
+      method,
+      body,
+      duplex,
+      ...opts.fetchOptions,
+      headers: fetchHeaders,
+    },
+    async onResponse(outputEvent, response) {
+
+ if (response.headers.get('Content-Type').includes('application/vnd.apple.mpegurl')) {
+
+  const arrayBuffer = await response.arrayBuffer();
+
+   let headersString = '';
+    for (const [key, value] of Object.entries(extractHeadersFromPath(outputEvent))) {
+      headersString += `&${key}=${encodeURIComponent(value)}`;
+    }
+    
+    const protocol = getRequestProtocol(outputEvent);
+    const hostname = getRequestHost(outputEvent);
+    const destination = getQuery<{ destination?: string }>(event).destination;
+
+    // Convert array buffer to string
+    const playlistText = new TextDecoder().decode(arrayBuffer);
+    const modifiedPlaylistText = playlistText
+      .split('\n')
+      .map((line) => {
+        if (line.startsWith('http')) {
+          // Add the proxy URL and referrer query parameter
+          const modifiedURL = `${protocol}//${hostname}/corsproxy/?destination=${encodeURIComponent(
+            line
+          )}${headersString}`;
+          return modifiedURL;
+        } else if (line.endsWith('m3u8')) {
+          // Modify playlist for vidsrcto
+          const modifiedURL = `?destination=${encodeURIComponent(
+            destination.replace(/\/list[^/]+\.m3u8/, '')
+          )}/${encodeURIComponent(line)}${headersString}`;
+
+          return modifiedURL;
+        } else if (line.endsWith('ts')) {
+          const modifiedURL = `?destination=${destination.replace(/\/[^/]+\.m3u8/, '')}/${encodeURIComponent(
+            line
+          )}${headersString}`;
+          return modifiedURL;
+        }
+        return line;
+      })
+      .join('\n');
+    
+      const modifiedArrayBuffer = new TextEncoder().encode(modifiedPlaylistText);
+      let modifiedResponse = new Response(modifiedArrayBuffer);
+
+      const headers = getAfterResponseHeaders(response.headers, response.url);
+      setResponseHeaders(outputEvent, headers);
+      sendWebResponse(outputEvent, modifiedResponse)
+    }
+  }
+      
+  });
+  
+}
